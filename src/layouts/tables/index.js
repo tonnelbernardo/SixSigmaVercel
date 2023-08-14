f7ce8ca87f3085bd4e7031a595898c9769783906
Tonnel <bernardo@sixsigma.com.br>
2023-08-14 12:28:16 -0300

    Atualiação Teste data 14

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -73,7 +73,7 @@ const Tables = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:2999/api/records'); 
+        const response = await axios.get('https://six-sigma-vercel-iisk-fi7e9yl6s-tonnelbernardo.vercel.app/api/records'); 
         console.log(response.data)
         setAuthors(response.data);
       } catch (error) {
