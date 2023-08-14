02606144b463bba40e3c1a4c97f94786d96feb36
Tonnel <bernardo@sixsigma.com.br>
2023-08-14 19:15:37 -0300

    Atualiação Teste data 23

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -23,93 +23,104 @@ const bgImage =
   "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg";
 
 
-function Login() {
-  const [cookies] = useCookies([]);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (cookies.jwt) {
-      navigate("/");
-    }
-  }, [cookies, navigate]);
-
-  const [values, setValues] = useState({ email: "", password: "" });
-  const generateError = (error) =>
-    toast.error(error, {
-      position: "bottom-right",
-    });
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const { data } = await axios.post(
-        "http://localhost:4000/api/login",
-        {
-          ...values,
-        },
-        { withCredentials: true }
-      );
-      if (data) {
-        if (data.errors) {
-          const { email, password } = data.errors;
-          if (email) generateError(email);
-          else if (password) generateError(password);
-        } else {
-          navigate("/");
-        }
+  function Login() {
+    const [cookies] = useCookies([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+      if (cookies.jwt) {
+        navigate("/");
       }
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+    }, [cookies, navigate]);
+  
+    const [values, setValues] = useState({ email: "", password: "" });
+    const generateError = (error) =>
+      toast.error(error, {
+        position: "bottom-right",
+      });
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const { data } = await axios.post(
+          "http://localhost:4000/api/login",
+          {
+            ...values,
+          },
+          { withCredentials: true }
+        );
+        if (data) {
+          if (data.errors) {
+            const { email, password } = data.errors;
+            if (email) generateError(email);
+            else if (password) generateError(password);
+          } else {
+            navigate("/");
+          }
+        }
+      } catch (ex) {
+        console.log(ex);
+      }
+    };
+  
     return (
       <div className='container'>
-      <IllustrationLayout
-      title="Sign In"
-      description="Enter your email and password to sign in"
-      illustration={{
-        image: bgImage,
-        title: '"Attention is the new currency"',
-        description:
-          "The more effortless the writing looks, the more effort the writer actually put into the process.",
-      }}
-    >
-      <form action="POST">
-        <ArgonBox mb={2}>
-          <ArgonInput type="email"             onChange={(e) =>
-              setValues({ ...values, [e.target.name]: e.target.value })
-            } placeholder="Email" size="large" />
-        </ArgonBox>
-        <ArgonBox mb={2}>
-          <ArgonInput type="password" onChange={(e) =>
-              setValues({ ...values, [e.target.name]: e.target.value })
-            } placeholder="Password" size="large" />
-        </ArgonBox>
-        <ArgonBox mt={4} mb={1}>
-          <ArgonButton type = "submit" color="info" onClick={(e) => handleSubmit(e)} size="large" fullWidth>
-            Sign In
-          </ArgonButton>
-        </ArgonBox>
-        <ArgonBox mt={3} textAlign="center">
-          <ArgonTypography variant="button" color="text" fontWeight="regular">
-            Don&apos;t have an account?{" "}
-            <ArgonTypography
-              component={Link}
-              to="/authentication/sign-up"
-              variant="button"
-              color="info"
-              fontWeight="medium"
-            >
-              Sign up
-            </ArgonTypography>
-          </ArgonTypography>
-        </ArgonBox>
-        </form>
-    </IllustrationLayout>
-    <ToastContainer />
-    </div>
-  );
-}
-
-export default Login
+        <IllustrationLayout
+          title="Sign In"
+          description="Enter your email and password to sign in"
+          illustration={{
+            image: bgImage,
+            title: '"Attention is the new currency"',
+            description:
+              "The more effortless the writing looks, the more effort the writer actually put into the process.",
+          }}
+        >
+          <form action="POST" onSubmit={handleSubmit}> {/* Usar onSubmit aqui */}
+            <ArgonBox mb={2}>
+              <ArgonInput
+                type="email"
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+                placeholder="Email"
+                size="large"
+              />
+            </ArgonBox>
+            <ArgonBox mb={2}>
+              <ArgonInput
+                type="password"
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+                placeholder="Password"
+                size="large"
+              />
+            </ArgonBox>
+            <ArgonBox mt={4} mb={1}>
+              <ArgonButton type="submit" color="info" size="large" fullWidth> {/* Remover o onClick */}
+                Sign In
+              </ArgonButton>
+            </ArgonBox>
+            <ArgonBox mt={3} textAlign="center">
+              <ArgonTypography variant="button" color="text" fontWeight="regular">
+                Don&apos;t have an account?{" "}
+                <ArgonTypography
+                  component={Link}
+                  to="/authentication/sign-up"
+                  variant="button"
+                  color="info"
+                  fontWeight="medium"
+                >
+                  Sign up
+                </ArgonTypography>
+              </ArgonTypography>
+            </ArgonBox>
+          </form>
+        </IllustrationLayout>
+        <ToastContainer />
+      </div>
+    );
+  }
+  
+  export default Login
 
 
 
